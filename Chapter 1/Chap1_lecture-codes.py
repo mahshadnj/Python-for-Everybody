201009dfa2acc74986670309605a98827d2fa6b3
Mahshad Nazari Jeirani <88314947+mahshadnj@users.noreply.github.com>
2025-01-12 16:38:34 +0330

    Add files via upload

---
+++ Chapter 1/Chap1_lecture-codes.py
@@ -0,0 +1,28 @@
+
+# reversed words:
+# You cannot use reversed words as variable names / identifiers
+#  False    await    else    import    pass
+#  None    break    except    in    raise
+
+# --------------
+
+x = 2
+print(x)
+# This will print 2
+
+x = x + 2
+print(x)
+# The output is 4
+
+# ---------------
+
+# A program for Conditional Steps
+x = 5
+if x < 10:
+    print('smaller')
+if x > 20:
+    print('Bigger')
+
+print('Finish')
+
+# ---------------
